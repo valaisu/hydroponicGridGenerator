1cc8379551e499df49c90f5066a5c0b77507362b
valaisu <hallman.valo@gmail.com>
2024-10-11 16:35:56 +0300

    Add function for moving

--- functions.py
+++ functions.py
@@ -5,6 +5,34 @@
 import bpy
 
 
+
+def move_vertices(object_name: str, vertex_group_names: list[str], move_vector: tuple[int, int, int]):
+
+    # Example usage:
+    # move_vertices("Cube", ["Group2", "Group"], (0, 0.5, 0))
+
+    # Select object
+    object = bpy.data.objects[object_name]
+    object.select_set(True)
+    bpy.context.view_layer.objects.active = object # sometimes needed
+
+    o = bpy.context.object  # "retrieves currently active object"
+
+    bpy.ops.object.mode_set( mode = 'EDIT' )
+    bpy.ops.mesh.select_all( action = 'DESELECT' )  # deselect vertices
+
+    for group_name in vertex_group_names:
+        o.vertex_groups.active = o.vertex_groups.get(group_name)
+        bpy.ops.object.vertex_group_select()
+
+    # this is what console prints out when I move using "g"-command
+    bpy.ops.transform.translate(value=move_vector, orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+
+    
+    bpy.ops.object.mode_set( mode = 'OBJECT' )
+    object.select_set(False)
+
+
 def bevel_vertex_group_edges(object_name: str, vertex_group_names: list[str], offset: float, segments: int = 1):
 
     # Example call:
