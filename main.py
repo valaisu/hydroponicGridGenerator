3b46734ecaeb36f0314f59ded3baa70693400740
valaisu <hallman.valo@gmail.com>
2024-10-11 20:17:35 +0300

    Add a basic main

---
+++ main.py
@@ -0,0 +1,65 @@
+import bpy
+from functions import *
+# Later code some kind of GUI
+
+BASE_PLATFORM_SIZE = 12
+
+MIN_PLATFORM_SIZE = 12
+MAX_PLATFORM_SIZE = 20
+
+PLATFORM_NAME = "platform"
+PLATFORM_PATH = "models/ModularPlatform.blend"
+SAVE_TO = "output/platform_ready.blend"
+
+
+def ask_container_size():
+    print(" Input the container dimensions in cm")
+    x = float(input("X: "))
+    y = float(input("Y: "))
+    return x, y
+
+
+def calc_grid_dim_limits(x, y):
+    x_max = round(x/MIN_PLATFORM_SIZE - 0.49999)
+    y_max = round(y/MIN_PLATFORM_SIZE - 0.49999)
+    x_min = round(x/MAX_PLATFORM_SIZE + 0.50000)
+    y_min = round(y/MAX_PLATFORM_SIZE + 0.50000)
+    print(f" Possible X counts: {x_min} - {x_max}, corresponding to platform sizes of {(x/x_max):.2f} - {(x/x_min):.2f} cm")
+    print(f" Possible Y counts: {y_min} - {y_max}, corresponding to platform sizes of {(y/y_max):.2f} - {(y/y_min):.2f} cm")
+    print(" Please choose the desired dimensions")
+    x_amount = int(input("X amount: "))
+    y_amount = int(input("Y amount: "))
+    x_size = x/x_amount
+    y_size = y/y_amount
+    return x_size, y_size, x_amount, y_amount
+
+
+def edit_platforms(x_size, y_size):
+
+    bpy.ops.wm.open_mainfile(filepath=PLATFORM_PATH)
+
+    # TODO: Edit the files here
+    # Note: at least 2 unique corner pieces are needed along with the base piece
+    
+    bpy.ops.wm.save_as_mainfile(filepath=SAVE_TO)
+
+    
+
+def main():
+
+    x, y = ask_container_size()
+    x_size, y_size, x_amount, y_amount = calc_grid_dim_limits(x, y)
+    print(f" Creating platforms of size {x_size:.2f}cm *{y_size:.2f}cm \n Amount to be printed: {x_amount} * {y_amount} = {x_amount*y_amount}")
+    #edit_platform(15, 15)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+
+
+# load objects from file
+
+
+
+
