047d1f465fc90c57daa48ee45412d9aa1e1a929e
valaisu <hallman.valo@gmail.com>
2024-10-11 22:51:13 +0300

    Use main to edit models
    Models are made with blender 4.0.0, as the lastest separate-from-blender-GUI version of bpm is 4.0.0

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ PLATFORM_PATH = "models/ModularPlatform.blend"
 SAVE_TO = "output/platform_ready.blend"
 
 
+PLATFORM_CORNER_VERT_GROUPS = ["RU", "LU", "RD", "LD"]
+MOVE_INSTRUCTIONS = {"RU" : (1, 1, 0), "LU" : (-1, 1, 0), "RD" : (1, -1, 0), "LD" : (-1, -1, 0)}
+
+
 def ask_container_size():
     print(" Input the container dimensions in cm")
     x = float(input("X: "))
@@ -36,10 +40,19 @@ def calc_grid_dim_limits(x, y):
 
 def edit_platforms(x_size, y_size):
 
+    x_move = (x_size - BASE_PLATFORM_SIZE) / 2
+    y_move = (y_size - BASE_PLATFORM_SIZE) / 2
+    move_instruction_multipliers = (x_move, y_move, 1)
+
     bpy.ops.wm.open_mainfile(filepath=PLATFORM_PATH)
 
     # TODO: Edit the files here
     # Note: at least 2 unique corner pieces are needed along with the base piece
+
+    # Get correct size
+    for name in PLATFORM_CORNER_VERT_GROUPS:
+        instr = tuple(a*b for a, b in zip(move_instruction_multipliers, MOVE_INSTRUCTIONS[name]))
+        move_vertices(PLATFORM_NAME, [name], instr)
     
     bpy.ops.wm.save_as_mainfile(filepath=SAVE_TO)
 
@@ -50,15 +63,12 @@ def main():
     x, y = ask_container_size()
     x_size, y_size, x_amount, y_amount = calc_grid_dim_limits(x, y)
     print(f" Creating platforms of size {x_size:.2f}cm *{y_size:.2f}cm \n Amount to be printed: {x_amount} * {y_amount} = {x_amount*y_amount}")
-    #edit_platform(15, 15)
+    edit_platforms(x_size, y_size)
     return 0
 
 
 if __name__ == "__main__":
     main()
-
-
-# load objects from file
 
 
 
